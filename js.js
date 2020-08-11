c3a44b89fa60ddc5f9bfd758fa26da9ee10d0415
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-08-11 14:16:04 +0900

    [ADD] Object Literal

--- js.js
+++ js.js
@@ -14,5 +14,21 @@ const address = {
 
 
 //배열 비구조화, Array Destructuring
-const [firstCountry, secondCountry, thridCountry] = ['Korea', 'Japan', 'America']
-console.log(firstCountry)
+// const [firstCountry, secondCountry, thirdCountry] = ['Korea', 'Japan', 'America']
+//이건 순서대로 명명 되는거
+// console.log(firstCountry)
+
+//객체 리터럴, Object Literal
+function getAddress(country, city, street){
+    const myAddress = { 
+        country : country, //이렇게 쓰는 건데 
+        city, //변수명이랑 같다면
+        street, //줄여 쓸 수 있다.
+        str_num : 111, //이렇게 추가 정의할 수도 있다.
+        postcode : '0920'
+    }
+    console.log(myAddress)
+}
+
+getAddress('Japan', 'Osaka', 'street')
+
