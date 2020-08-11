67a01463b8fb055c9b6cb4d061a1c6f8b1607fd3
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-08-11 14:11:05 +0900

    [ADD] Array Destructuring

--- js.js
+++ js.js
@@ -6,9 +6,13 @@ const address = {
     postcode : '00100'
 }
 
-//객체 비구조화, Object Destucturing
-const country = 'Japan'
-const city = 'Osaka'
+//객체 비구조화, Object Destructuring
+// const country = 'Japan'
 
-const { country:nextCountry} = address //rename 가능
-console.log(`${country}, ${nextCountry}`)
+// const { country:nextCountry} = address //rename 가능
+// console.log(`${country}, ${nextCountry}`)
+
+
+//배열 비구조화, Array Destructuring
+const [firstCountry, secondCountry, thridCountry] = ['Korea', 'Japan', 'America']
+console.log(firstCountry)
